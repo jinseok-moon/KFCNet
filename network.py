1ef65169ffcaa106b48089f7bf49a90c45a8ba8a
Jinseok Moon <jsmoon.dev@gmail.com>
2022-04-07 19:55:33 +0900

    resnet50 - layer1

--- network.py
+++ network.py
@@ -13,26 +13,14 @@ from torchvision import datasets, transforms
 class KFCNet(nn.Module):
     def __init__(self):
         super(KFCNet, self).__init__()
-        # b, 3, 255, 255
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, padding=4),  # b, 96, 55, 55
-            nn.ReLU(),
-            nn.Dropout2d(0.5),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.MaxPool2d(kernel_size=(4, 4))
-        )
-
-        self.layer2 = nn.Sequential(
-            nn.Linear(in_features=32 * 6 * 6, out_features=128),
-            nn.Linear(in_features=128, out_features=27)  # number of food classes
+            nn.Conv2d(3, 64, 7, 2, 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2, 1)
         )
 
     def forward(self, x):
         x = self.layer1(x)
         x = x.view(x.size(0), -1)  # Flatten them for FC
-        x = self.layer2(x)
         return x
